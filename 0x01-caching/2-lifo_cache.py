1828b4d0e76089b6ca517c9e44232b4566595765
Frankie Vexx <franblog7@gmail.com>
2023-07-26 14:45:58 +0300

    LIFOCache class that inherits from BaseCache

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""Create a class LIFOCache that inherits from
+BaseCaching and is a caching system:
+"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFOCache class inheriting from BaseCaching
+    """
+
+    def __init__(self):
+        """_init_ function
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """Put function
+        """
+        if key is None or item is None:
+            pass
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data.keys():
+                # delete the last item in the dictionary
+                last_key, last_value = self.cache_data.popitem()
+                print("DISCARD: {}". format(last_key))
+
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """return the value in self.cache_data linked to key
+
+        Args:
+                        key (_type_): _description_
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
