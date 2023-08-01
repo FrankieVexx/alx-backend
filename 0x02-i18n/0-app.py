fc4ede3603d2dfe83feb50cdf0ee916906aace6f
Frankie Vexx <franblog7@gmail.com>
2023-08-01 17:55:22 +0300

    Fixed documentation

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,17 @@
+#!/usr/bin/env python3
+"""CReate a basic flask app"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
 def index():
+    """The index page to be served
+    """
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
