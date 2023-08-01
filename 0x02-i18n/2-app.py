d4f3c5d3ca5338f06fbe990e184148bbb85e5ccd
Frankie Vexx <franblog7@gmail.com>
2023-08-01 18:21:16 +0300

    fixed pycode

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -20,7 +20,7 @@ babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale()-> str:
+def get_locale() -> str:
     """get locale language
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
